4d8ee9f5ebeec28d0c9212f6725f2fd19c730048
Yash Verma <yashverma056@proton.me>
2026-02-04 11:41:30 -0500

    Fix Makefiles and run script. Also added support for Limine v10 to the kernel

--- Core/PreBoot/Vendors/BOOTVENDOR_Limine.c
+++ Core/PreBoot/Vendors/BOOTVENDOR_Limine.c
@@ -1,34 +1,50 @@
+#include "Boot/Vendors/Limine.h"
 #include <__KCONF.h>
 #include <Boot/PreBoot.h>
 
-#define Requested __attribute__((used, section(".requests")))
+#define __limine_requests __attribute__((used, section(".requests")))
+#define __limine_requests_start __attribute__((used, section(".requests_start")))
+#define __limine_requests_end __attribute__((used, section(".requests_end")))
 
 #ifdef BOOTVENDOR_Limine
-    /*Services*/
-    Requested
+    __limine_requests
+    volatile uint64_t limineBaseRevision[] = LIMINE_BASE_REVISION(4);
+
+    __limine_requests_start
+    volatile uint64_t limineRequestsStartMarker[] = LIMINE_REQUESTS_START_MARKER;
+
+    __limine_requests_end
+    volatile uint64_t LimineRequestsEndMarker[] = LIMINE_REQUESTS_END_MARKER;
+
+    /* Services */
+    __limine_requests
     volatile struct limine_hhdm_request ReqHHDM=
     {
-        .id = LIMINE_HHDM_REQUEST, .revision = LimineRevision
+        .id = LIMINE_HHDM_REQUEST_ID, .revision = LimineRevision
     };  
-    Requested
+    
+    __limine_requests
     volatile struct limine_memmap_request ReqMEMORYMAP=
     {
-        .id = LIMINE_MEMMAP_REQUEST, .revision = LimineRevision
+        .id = LIMINE_MEMMAP_REQUEST_ID, .revision = LimineRevision
     };  
-    Requested
+    
+    __limine_requests
     volatile struct limine_framebuffer_request ReqFRAMEBUFFER=
     {
-        .id = LIMINE_FRAMEBUFFER_REQUEST, .revision = LimineRevision
-    };  
-    Requested
-    volatile struct limine_smp_request ReqSMP=
+        .id = LIMINE_FRAMEBUFFER_REQUEST_ID, .revision = LimineRevision
+    };
+
+    __limine_requests
+    volatile struct limine_mp_request ReqSMP=
     {
-        .id = LIMINE_SMP_REQUEST, .revision = LimineRevision
-    };  
-    Requested
+        .id = LIMINE_MP_REQUEST_ID, .revision = LimineRevision
+    };
+
+    __limine_requests
     volatile struct limine_module_request ReqMODULE=
     {
-        .id = LIMINE_MODULE_REQUEST, .revision = LimineRevision
+        .id = LIMINE_MODULE_REQUEST_ID, .revision = LimineRevision
     };
 
     /*Request*/
